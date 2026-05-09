b1282aad1e078cdd2364f42933b03194837e2979
4mzeynali <mzeynali01@gmail.com>
2026-05-09 23:54:01 +0330

    01-langgraph

--- 05-capstone/integrations/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Real Gmail / GCal / CRM integrations for the capstone.
-
-These modules are intentionally *scaffolds*. They work for the basic cases
-shown in the README but have `# TODO(you)` markers where you should add
-handling as you hit real-world edge cases (pagination, rate limits, etc.).
-"""
